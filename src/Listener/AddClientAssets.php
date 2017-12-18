1d42d3b7c161111311dc258ffdc02b5a2b581074
Admin <admin@Admins-MacBook-Pro.local>
2017-12-18 15:55:58 +0800

    fix the issue:  the event listen to is deprecated - fixing types

--- src/Listener/AddClientAssets.php
+++ src/Listener/AddClientAssets.php
@@ -28,8 +28,7 @@ class AddClientAssets
     {
         if ($event->isForum()) {
             $event->addAssets([
-                __DIR__ . '/../../less/forum/upload.less',
-                __DIR__ . '/../../less/forum/download.less',
+                __DIR__ . '/../../less/forum/extension.less',
                 __DIR__ . '/../../js/forum/dist/extension.js'
             ]);
             $event->addBootstrapper('wiwatSrt/bestAnswer/main');
